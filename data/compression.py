97e9c7badf0e1fa8224617865240ffda248fe7ba
Lint Action <lint-action@samuelmeuli.com>
2021-12-25 11:10:12 +0000

    Fix code style issues with Black

--- data/compression.py
+++ data/compression.py
@@ -10,7 +10,7 @@ from loguru import logger
 
 def save_file(file_name, data):
     """Save data to a file."""
-    with bz2.BZ2File(file_name, 'wb') as f:
+    with bz2.BZ2File(file_name, "wb") as f:
         cPickle.dump(data, f)
     return file_name
 
@@ -18,62 +18,62 @@ def save_file(file_name, data):
 def compress_files():
     """Compress all files in data."""
     files_path = []
-    for path, subdirs, files in os.walk('.'):
+    for path, subdirs, files in os.walk("."):
         for name in files:
             files_path.append(pathlib.PurePath(path, name))
     csv_files = []
     pickle_files = []
     for file in files_path:
-        if file.endswith(('.csv')):
+        if file.endswith((".csv")):
             csv_files.append(file)
-        if file.endswith(('.pickle', '.pkl')):
+        if file.endswith((".pickle", ".pkl")):
             pickle_files.append(file)
 
     if csv_files:
         for csv_file in csv_files:
             df = pd.read_csv(csv_file)
-            df.to_csv(csv_file + '.gzip', compression='gzip', index=None)
+            df.to_csv(csv_file + ".gzip", compression="gzip", index=None)
 
     if pickle_files:
         for pickle_file in pickle_files:
-            with open(pickle_file, 'rb') as handle:
+            with open(pickle_file, "rb") as handle:
                 data = pickle.load(handle)
-                save_file(pickle_file + '.pbz2', data)
+                save_file(pickle_file + ".pbz2", data)
 
 
 def decompress_files():
     """Decompress all files in data."""
     files_path = []
-    for path, subdirs, files in os.walk('.'):
+    for path, subdirs, files in os.walk("."):
         for name in files:
             files_path.append(pathlib.PurePath(path, name))
     zipped_csv_files = []
     zipped_pickle_files = []
     for file in files_path:
-        if str(file).endswith(('.gzip')):
+        if str(file).endswith((".gzip")):
             zipped_csv_files.append(file)
-        if str(file).endswith(('.pbz2')):
+        if str(file).endswith((".pbz2")):
             zipped_pickle_files.append(file)
 
     if zipped_csv_files:
         for csv_file in zipped_csv_files:
-            df = pd.read_csv(csv_file, compression='gzip')
-            df.to_csv(str(csv_file).replace('.gzip', ""), index=None)
+            df = pd.read_csv(csv_file, compression="gzip")
+            df.to_csv(str(csv_file).replace(".gzip", ""), index=None)
             if os.path.isfile(csv_file):
                 os.remove(csv_file)
             else:  # Show an error
-                logger.error(f'Error: {csv_file} file not found')
+                logger.error(f"Error: {csv_file} file not found")
 
     if zipped_pickle_files:
         for pickle_file in zipped_pickle_files:
-            data = bz2.BZ2File(pickle_file, 'rb')
+            data = bz2.BZ2File(pickle_file, "rb")
             data = cPickle.load(data)
-            with open(str(pickle_file).replace('.pbz2', ""), 'wb') as handle:
+            with open(str(pickle_file).replace(".pbz2", ""), "wb") as handle:
                 pickle.dump(data, handle, protocol=pickle.HIGHEST_PROTOCOL)
             if os.path.isfile(pickle_file):
                 os.remove(pickle_file)
             else:  # Show an error ##
-                logger.error(f'Error: {pickle_file} file not found')
+                logger.error(f"Error: {pickle_file} file not found")
 
 
 def decompress_file(
@@ -95,30 +95,30 @@ def decompress_file(
     """
     if not file.is_file():
         return None
-    if file.suffix == '.gzip':
-        df = pd.read_csv(file, compression='gzip')
+    if file.suffix == ".gzip":
+        df = pd.read_csv(file, compression="gzip")
         save_as = file.parent if extract_to is None else extract_to
         save_as.mkdir(parents=True, exist_ok=True)
         save_as = save_as / (file.stem if new_file_name is None else new_file_name)
         if save_as.exists():
             save_as.unlink()
         df.to_csv(save_as, index=None)
-    elif file.suffix == '.pbz2':
-        data = bz2.BZ2File(file, 'rb')
+    elif file.suffix == ".pbz2":
+        data = bz2.BZ2File(file, "rb")
         data = cPickle.load(data)
         save_as = file.parent if extract_to is None else extract_to
         save_as.mkdir(parents=True, exist_ok=True)
         save_as = save_as / (file.stem if new_file_name is None else new_file_name)
         if save_as.exists():
             save_as.unlink()
-        with open(save_as, 'wb') as handle:
+        with open(save_as, "wb") as handle:
             pickle.dump(data, handle, protocol=pickle.HIGHEST_PROTOCOL)
     else:  # file not supported
         return None
-    logger.info(f'Extracted to {save_as}')
+    logger.info(f"Extracted to {save_as}")
     if delete_compressed:
         if file.is_file():
             file.unlink()
         else:
-            logger.error(f'Error: {file} file not found')
+            logger.error(f"Error: {file} file not found")
     return save_as
