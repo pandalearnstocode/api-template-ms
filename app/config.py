5c3af231280625098c294a8d3b9718bc0ab0d388
Lint Action <lint-action@samuelmeuli.com>
2022-04-08 00:55:44 +0000

    Fix code style issues with oitnb

--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ from pydantic import BaseSettings, Field
 
 
 class Settings(BaseSettings):
-    db_url: str = Field(..., env="DATABASE_URL")
+    db_url: str = Field(..., env='DATABASE_URL')
 
 
 settings = Settings()
