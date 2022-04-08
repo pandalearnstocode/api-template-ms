3f327c054e86e24f2ba3ad6685a94dc1eba1193d
Lint Action <lint-action@samuelmeuli.com>
2022-04-08 00:55:43 +0000

    Fix code style issues with Black

--- movie-service/app/api/db.py
+++ movie-service/app/api/db.py
@@ -4,19 +4,19 @@ from sqlalchemy import ARRAY, Column, Integer, MetaData, String, Table, create_e
 
 from databases import Database
 
-DATABASE_URI = os.getenv('DATABASE_URI')
+DATABASE_URI = os.getenv("DATABASE_URI")
 
 engine = create_engine(DATABASE_URI)
 metadata = MetaData()
 
 movies = Table(
-    'movies',
+    "movies",
     metadata,
-    Column('id', Integer, primary_key=True),
-    Column('name', String(50)),
-    Column('plot', String(250)),
-    Column('genres', ARRAY(String)),
-    Column('casts_id', ARRAY(Integer)),
+    Column("id", Integer, primary_key=True),
+    Column("name", String(50)),
+    Column("plot", String(250)),
+    Column("genres", ARRAY(String)),
+    Column("casts_id", ARRAY(Integer)),
 )
 
 database = Database(DATABASE_URI)
