010b337140a0a72eeba045d66e608a88d1485abc
Lint Action <lint-action@samuelmeuli.com>
2022-05-23 23:28:30 +0000

    Fix code style issues with oitnb

--- movie-service/app/main.py
+++ movie-service/app/main.py
@@ -4,17 +4,17 @@ from app.api.db import metadata, database, engine
 
 metadata.create_all(engine)
 
-app = FastAPI(openapi_url="/api/v1/movies/openapi.json", docs_url="/api/v1/movies/docs")
+app = FastAPI(openapi_url='/api/v1/movies/openapi.json', docs_url='/api/v1/movies/docs')
 
 
-@app.on_event("startup")
+@app.on_event('startup')
 async def startup():
     await database.connect()
 
 
-@app.on_event("shutdown")
+@app.on_event('shutdown')
 async def shutdown():
     await database.disconnect()
 
 
-app.include_router(movies, prefix="/api/v1/movies", tags=["movies"])
+app.include_router(movies, prefix='/api/v1/movies', tags=['movies'])
