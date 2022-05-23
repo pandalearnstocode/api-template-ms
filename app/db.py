bd90700b051f93780b789449ae00b516697dac22
Lint Action <lint-action@samuelmeuli.com>
2022-05-23 23:17:56 +0000

    Fix code style issues with oitnb

--- app/db.py
+++ app/db.py
@@ -1,7 +1,7 @@
 import os
 from sqlmodel import create_engine, SQLModel, Session
 
-DATABASE_URL = os.environ.get("DATABASE_URL")
+DATABASE_URL = os.environ.get('DATABASE_URL')
 engine = create_engine(DATABASE_URL, echo=True)
 
 
