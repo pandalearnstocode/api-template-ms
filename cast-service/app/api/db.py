f14b89d9059caa4666a008f978ca6547df36cc8c
Lint Action <lint-action@samuelmeuli.com>
2022-07-13 21:53:38 +0000

    Fix code style issues with oitnb

--- cast-service/app/api/db.py
+++ cast-service/app/api/db.py
@@ -4,17 +4,17 @@ from sqlalchemy import Column, Integer, MetaData, String, Table, create_engine
 
 from databases import Database
 
-DATABASE_URI = os.getenv("DATABASE_URI")
+DATABASE_URI = os.getenv('DATABASE_URI')
 
 engine = create_engine(DATABASE_URI)
 metadata = MetaData()
 
 casts = Table(
-    "casts",
+    'casts',
     metadata,
-    Column("id", Integer, primary_key=True),
-    Column("name", String(50)),
-    Column("nationality", String(20)),
+    Column('id', Integer, primary_key=True),
+    Column('name', String(50)),
+    Column('nationality', String(20)),
 )
 
 database = Database(DATABASE_URI)
